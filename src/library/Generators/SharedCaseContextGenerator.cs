6bf1ba4b7e45651e792a3f24984aa0d9a3ae187e
Jeremiah Sanders <1328897+JeremiahSanders@users.noreply.github.com>
2024-11-11 22:13:45 -0600

    feat: add support for record shared case context

--- src/library/Generators/SharedCaseContextGenerator.cs
+++ src/library/Generators/SharedCaseContextGenerator.cs
@@ -23,6 +23,20 @@ public class SharedCaseContextGenerator : ISourceGenerator
   {
     if (context.SyntaxReceiver is SyntaxReceiver receiver)
     {
+      foreach (var recordDeclaration in receiver.CandidateRecords.Distinct())
+      {
+        var (collectionHintName, collectionSource) =
+          SharedContextCollectionGeneration.CreateCollectionSource(recordDeclaration);
+        context.AddSource(collectionHintName, collectionSource);
+
+        var (assertionsHintName, assertionsSource) =
+          SharedCaseAssertionsGeneration.CreateAssertionsSource(recordDeclaration);
+        context.AddSource(assertionsHintName, assertionsSource);
+
+        var (fixtureHintName, fixtureSource) = SharedCaseFixtureGeneration.CreateFixtureSource(recordDeclaration);
+        context.AddSource(fixtureHintName, fixtureSource);
+      }
+
       foreach (var classDeclaration in receiver.CandidateClasses.Distinct())
       {
         var (collectionHintName, collectionSource) =
@@ -43,12 +57,27 @@ public class SharedCaseContextGenerator : ISourceGenerator
   private class SyntaxReceiver : ISyntaxReceiver
   {
     public List<ClassDeclarationSyntax> CandidateClasses { get; } = new();
+    public List<RecordDeclarationSyntax> CandidateRecords { get; } = new();
 
     public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
     {
       const string shortName = "SharedCaseContext";
       const string attributeName = "SharedCaseContextAttribute";
-      if (syntaxNode is ClassDeclarationSyntax { AttributeLists.Count: > 0 } classDeclarationSyntax)
+      if (syntaxNode is RecordDeclarationSyntax { AttributeLists.Count: > 0 } recordDeclarationSyntax)
+      {
+        foreach (var attributeList in recordDeclarationSyntax.AttributeLists)
+        {
+          foreach (var attribute in attributeList.Attributes)
+          {
+            if (attribute.Name.ToString() is shortName or attributeName)
+            {
+              CandidateRecords.Add(recordDeclarationSyntax);
+              break;
+            }
+          }
+        }
+      }
+      else if (syntaxNode is ClassDeclarationSyntax { AttributeLists.Count: > 0 } classDeclarationSyntax)
       {
         foreach (var attributeList in classDeclarationSyntax.AttributeLists)
         {
